aa6385432e362e695f4c2a7f322ad5b94997d0c8
Dharwin R V J <dharwinruppa1422@gmail.com>
2022-05-14 18:06:07 +0530

    ci4

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -34,7 +34,7 @@ class Database extends Config
 		'DSN'      => '',
 		'hostname' => 'localhost',
 		'username' => 'root',
-		'password' => 'Localhost123@',
+		'password' => 'D123@localhost',
 		'database' => 'ci4crud',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
